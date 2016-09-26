c406e1e3ccbcd8ca4fd173ae6aa2b396bfb179dc
bidah <bartoindahouse@gmail.com>
2016-09-26 15:14:05 -0300

    done

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -7,11 +7,30 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+//    for (NSUInteger i = 1; i <= 100; i++) {
+//        NSString *result = @"";
+//        result = [ NSString stringWithFormat:@"%lu", i ];
+//        NSLog(@"%@", result);
+//    }
+    
+    NSUInteger start = 100;
+    NSUInteger finish = 130;
+    NSUInteger fizzIncrement = 3;
+    NSUInteger buzzIncrement = 5;
+    
+    for (NSUInteger i = start; i <= finish; i++) {
+        NSString *result = @"";
+        if (i % fizzIncrement == 0 && i % buzzIncrement == 0) {
+            result = @"FizzBuzz";
+        } else if (i % fizzIncrement == 0) {
+            result = @"Fizz";
+        } else if (i % buzzIncrement == 0) {
+            result = @"Buzz";
+        } else {
+            result = [ NSString stringWithFormat:@"%lu", i ];
+        }
+        NSLog(@"%@", result);
+    }
     
     // do not alter
     return YES;  //
